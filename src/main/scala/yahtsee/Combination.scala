cf40cce49a5e552e63687f9b05b9b5d13406278d
Benoit <bpasquereau@gmail.com>
2019-10-24 20:56:48 +0100

    Added support for 'Upper'

--- src/main/scala/yahtsee/Combination.scala
+++ src/main/scala/yahtsee/Combination.scala
@@ -3,9 +3,16 @@ package yahtsee
 object Dice extends Enumeration {
   type Dice = Value
   val One, Two, Three, Four, Five, Six = Value
+  def getValue(d:Dice) = d.id + 1
 }
 
 import Dice.Dice
+
+object Roll {
+  type Roll = (Dice,Dice,Dice,Dice,Dice)
+}
+
+import Roll.Roll
 
 sealed abstract class Combination extends Product with Serializable
 
@@ -19,5 +26,9 @@ object Combination {
   final case object Chance extends Combination
   final case object Yahtsee extends Combination
 
-//  def getPoints(c: Combination, r:Roll) : Int 
+  type Points = Int
+
+  def getPoints(c: Combination, r:Roll) : Points = c match {
+    case Upper(d) => { r.productIterator.map(_.asInstanceOf[Dice.Value]).filter(d2 =>  (d2 == d)).toList.length * Dice.getValue(d) }
+  }
 }
