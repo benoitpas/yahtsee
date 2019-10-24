b509a583abd4dbc945410b6fce8d8133020c9566
Benoit <bpasquereau@gmail.com>
2019-10-24 07:32:29 +0100

    Added type Dice

--- src/main/scala/yahtsee/Combination.scala
+++ src/main/scala/yahtsee/Combination.scala
@@ -1,9 +1,16 @@
 package yahtsee
 
+object Dice extends Enumeration {
+  type Dice = Value
+  val One, Two, Three, Four, Five, Six = Value
+}
+
+import Dice.Dice
+
 sealed abstract class Combination extends Product with Serializable
 
 object Combination {
-  final case class Upper(diceValue: Int) extends Combination
+  final case class Upper(diceValue: Dice) extends Combination
   final case object ThreeOfAKind extends Combination
   final case object FourOfAKind extends Combination
   final case object FullHouse extends Combination
