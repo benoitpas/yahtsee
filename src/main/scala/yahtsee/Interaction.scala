17f6e50170fc5e6656ccf5dd0c4c26273bebc646
Benoit <bpasquereau@gmail.com>
2019-11-16 17:37:35 +0000

    Improved generateRoll3

--- src/main/scala/yahtsee/Interaction.scala
+++ src/main/scala/yahtsee/Interaction.scala
@@ -1,6 +1,7 @@
 package yahtsee
 
 import cats.effect.IO
+import cats.implicits._ 
 
 object Interaction {
     def readIntList(): IO[List[Int]] = {
@@ -10,10 +11,19 @@ object Interaction {
 
     def readIntList(io:IO[String]) : IO[List[Int]] = for {
         l <- io
-    } yield l match {
+    } yield l.trim match {
         case "" => List()
-        case _ => l.split(",").map(_.toInt).toList
+        case l2 => l2.split(",").toList.map(_.toInt)
     }
+    
+/*
+    // Better error handling
+    def readIntList(io:IO[String]) : IO[Either[NumberFormatException,List[Int]]] = for {
+        l <- io
+    } yield l match {
+        case "" => Either.right(List())
+        case _ => l.split(",").toList.traverse((s:String) => Either.catchOnly[NumberFormatException](s.toInt))
+    }*/
 
 
     // Simple unpure implementation
@@ -51,12 +61,26 @@ object Interaction {
         )
     }
 
+    // Use 'for' instead of flatMap on IO monad
+    def generateRoll2b(s1:Seed) : IO[(Seed,Roll)] = {
+        val r1 = RandomGen.nextRoll.run(s1).value
+        for {
+            _ <- IO {println(r1._2)}
+            l1 <- readIntList
+            r2 = RandomGen.updateRoll(r1._2,l1).run(r1._1).value
+            _ <- IO {println(r2._2)}
+            l2 <- readIntList
+            r3 = RandomGen.updateRoll(r2._2,l2).run(r2._1).value
+            _ <- IO { println(r3._2)}
+        } yield r3
+    }
+
     // trying to use StateT monad transformer to mix IO and Seed monads (pure)
     def firstRoll(seed: Seed) : IO[(Seed,Roll)] = IO {
         RandomGen.nextRoll.run(Seed()).value
     }
 
-    def request(r: Roll, seed: Seed): IO[(Seed,Roll)] = 
+    def updateRoll(r: Roll, seed: Seed): IO[(Seed,Roll)] = 
         for {
             _ <- IO {println(r) }
             l <- readIntList()
@@ -64,19 +88,18 @@ object Interaction {
 
 
     import cats.data.StateT
-    def requestWithState(r: Roll): StateT[IO, Seed, (Seed,Roll)] = for {
+    def updateRollWithState(r: Roll): StateT[IO, Seed, (Seed,Roll)] = for {
         seed <- StateT.get[IO, Seed]
-        resp <- StateT.liftF(request(r, seed))
+        resp <- StateT.liftF(updateRoll(r, seed))
         _ <- StateT.modify[IO, Seed](_ => resp._1)
     } yield resp
 
-    def generateRoll3(s:Seed) = {
-        (for {
+    def generateRoll3 = 
+        for {
             seed <- StateT.get[IO, Seed]
             resp1 <- StateT.liftF(firstRoll(seed))
             _ <- StateT.modify[IO,Seed](_ => resp1._1)
-            resp2 <- requestWithState(resp1._2)
-            resp3 <- requestWithState(resp2._2)
-         } yield resp3).run(s)
-    }
+            resp2 <- updateRollWithState(resp1._2)
+            resp3 <- updateRollWithState(resp2._2)
+         } yield resp3
 }
--- src/main/scala/yahtsee/InteractionApp.scala
+++ src/main/scala/yahtsee/InteractionApp.scala
@@ -4,6 +4,7 @@ object InteractionApp extends App {
     val s = Seed()
     println(Interaction.generateRoll(s).unsafeRunSync())
     println(Interaction.generateRoll2(s).unsafeRunSync())
-    println(Interaction.generateRoll3(s).unsafeRunSync())
+    println(Interaction.generateRoll2b(s).unsafeRunSync())
+    println(Interaction.generateRoll3.run(s).unsafeRunSync())
 
 }
